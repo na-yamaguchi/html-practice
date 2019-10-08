def755badd122ee7cbea65a839bff3c98704cbe5
na.yamaguchi <na.yamaguchi@eimee.co.jp>
2019-10-08 14:15:32 +0900

    chore

---
+++ pages/slack.vue
@@ -0,0 +1,656 @@
+<template>
+  <div class="container">
+    <aside>
+      <div class="aside-header">
+        <div class="workspace">
+          <p>Eimee</p>
+          <img src="/down-icon.svg" />
+        </div>
+        <button class="bell">
+          <img src="/pin-icon.png" />
+        </button>
+        <div class="user-name">
+          <span class="online" />
+          <p>山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+        </div>
+      </div>
+      <div class="aside-buttons">
+        <button class="change-channel">
+          <img src="/pin-icon.png" />
+          会話を移動
+        </button>
+        <button class="preview-channel">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="next-channel">
+          <img src="/pin-icon.png" />
+        </button>
+      </div>
+      <div class="aside-scrole">
+        <div class="sled-link">
+          <img src="/pin-icon.png" />
+          <p>スレッド</p>
+        </div>
+        <div class="channels">
+          <p>スター付き</p>
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="channels">
+          <p>チャンネル</p>
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="channels">
+          <p>ダイレクトメッセージ</p>
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="invite-link">
+          <img src="/pin-icon.png" />
+          <p>メンバーを招待</p>
+        </div>
+        <div class="channels">
+          <p>App</p>
+          <img src="/pin-icon.png" />
+        </div>
+      </div>
+    </aside>
+    <div class="main-header">
+      <div class="header-title">
+        <p>#分報_山口奈津美</p>
+      </div>
+      <div class="header-buttons">
+        <button class="star">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="user">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="pin">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="topic">
+          <img src="/pin-icon.png" />
+          トピックを追加
+        </button>
+      </div>
+      <div class="header-tools">
+        <button class="tel">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="info">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="setting">
+          <img src="/pin-icon.png" />
+        </button>
+      </div>
+    </div>
+    <div class="sub-header">
+      <div class="header-search">
+        <img src="/pin-icon.png" />
+        <input type="text-box" />
+      </div>
+      <div class="header-tools">
+        <button class="mention">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="star">
+          <img src="/pin-icon.png" />
+        </button>
+        <button class="othor">
+          <img src="/pin-icon.png" />
+        </button>
+      </div>
+    </div>
+    <main>
+      <div class="date-line">
+        <p>昨日</p>
+      </div>
+      <div class="main-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+        <div class="sled-open">
+          <img src="/pin-icon.png" class="user-icon" />
+          <a href="#">1件の返信</a>
+          <span>1日前</span>
+        </div>
+      </div>
+      <div class="main-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+        <div class="sled-open">
+          <img src="/pin-icon.png" class="user-icon" />
+          <a href="#">1件の返信</a>
+          <span>1日前</span>
+        </div>
+      </div>
+      <div class="date-line">
+        <p>今日</p>
+      </div>
+      <div class="main-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+        <div class="sled-open">
+          <img src="/pin-icon.png" class="user-icon" />
+          <a href="#">1件の返信</a>
+          <span>1日前</span>
+        </div>
+      </div>
+      <div class="main-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+        <div class="sled-open">
+          <img src="/pin-icon.png" class="user-icon" />
+          <a href="#">1件の返信</a>
+          <span>1日前</span>
+        </div>
+      </div>
+    </main>
+    <div class="submit">
+      <div class="submit-area">
+        <div class="icon">
+          <img src="/pin-icon.png" class="clip-icon" />
+        </div>
+        <input type="text-box" />
+        <div class="icon">
+          <img src="/pin-icon.png" class="mention-icon" />
+        </div>
+        <div class="icon">
+          <img src="/pin-icon.png" class="smile-icon" />
+        </div>
+      </div>
+      <button class="submit-button">送信</button>
+    </div>
+    <div class="sled-header">
+      <div class="sled-title">
+        <p>スレッド</p>
+      </div>
+      <div class="channel-name">
+        <p>#分報_山口奈津美</p>
+      </div>
+      <button class="sled-close">
+        <img src="/pin-icon.png" />
+      </button>
+    </div>
+    <div class="sled-area">
+      <div class="sled-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+      </div>
+      <div class="reply-line">
+        <p>1件の返信</p>
+      </div>
+      <div class="sled-message">
+        <div class="profile-icon">
+          <img src="/pin-icon.png" />
+        </div>
+        <div class="user-status">
+          <p class="name">山口 奈津美</p>
+          <span>&#x1f3e2;</span>
+          <p class="time">10:58</p>
+        </div>
+        <div class="text">
+          テキスト
+        </div>
+        <div class="reactions">
+          <button>
+            <span class="reaction">&#x1f914;</span>
+            1
+          </button>
+          <button>
+            <span class="reaction">&#x1f60e;</span>
+            1
+          </button>
+        </div>
+      </div>
+      <div class="sled-submit">
+        <div class="submit-area">
+          <div class="icon">
+            <img src="/pin-icon.png" class="clip-icon" />
+          </div>
+          <input type="text-box" />
+          <div class="icon">
+            <img src="/pin-icon.png" class="smile-icon" />
+          </div>
+        </div>
+        <div class="and-channel">
+          <label>
+            <input type="checkbox" />
+            <p>
+              以下にも投稿する:
+              <strong>
+                ♯分報_山口奈津美
+              </strong>
+            </p>
+          </label>
+          <a href="#">
+            <img src="/pin-icon.png" class="question-icon" />
+          </a>
+        </div>
+        <button class="submit-button">送信</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 220px 1fr 450px;
+  grid-template-rows: 60px 60px 1fr 60px;
+  font-family: "Yu Gothic", san-serif;
+
+  img {
+    weight: 20px;
+    height: 20px;
+  }
+
+  aside {
+    grid-column: 1 / 2;
+    grid-row: 1 / 5;
+    background-color: #4d5250;
+    color: #ffffff;
+
+    .aside-header {
+      padding: 10px;
+      display: grid;
+      grid-template-columns: 1fr 30px;
+      grid-template-rows: 1.2em 1.2em;
+      opacity: 0.6;
+
+      &:hover {
+        opacity: 1;
+        background-color: #444a47;
+      }
+
+      .workspace {
+        display: flex;
+        align-items: center;
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+
+        p {
+          font-weight: 900;
+          font-size: 18px;
+          letter-spacing: -1px;
+        }
+
+        img {
+          width: 10px;
+          height: 10px;
+          margin: 8px;
+        }
+      }
+
+      button.bell {
+        grid-column: 2 / 3;
+        grid-row: 1 / 2;
+        border: none;
+        background-color: red;
+        img {
+        }
+      }
+
+      .user-name {
+        grid-column: 1 / 3;
+        grid-row: 2 / 3;
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+
+        .online {
+          background-color: #99d04a;
+          width: 10px;
+          height: 10px;
+          border-radius: 5px;
+        }
+        .offline {
+        }
+
+        p {
+          letter-spacing: -1px;
+          color: #c7c9c8;
+          font-weight: 900;
+          margin: 0 5px;
+        }
+        span {
+        }
+      }
+    }
+
+    .aside-buttons {
+      display: flex;
+      margin: 4px 10px;
+      button {
+        border: none;
+        background-color: #686c6a;
+        height: 28px;
+      }
+      img {
+        width: 10px;
+        height: 10px;
+      }
+      button.change-channel {
+        font-size: 15px;
+        font-weight: 300;
+        border-radius: 3px;
+        margin-right: 10px;
+        width: 100%;
+        text-align: left;
+        color: #bbc2c2;
+        img {
+        }
+      }
+      button.preview-channel {
+        border-radius: 3px 0 0 3px;
+        width: 40px;
+        img {
+        }
+      }
+      button.next-channel {
+        border-radius: 0 3px 3px 0;
+        width: 40px;
+        opacity: 0.5;
+        img {
+        }
+      }
+    }
+    .aside-scrole {
+      margin: 10px;
+      .sled-link {
+        display: flex;
+        img {
+        }
+      }
+      .channels {
+        display: flex;
+        p {
+        }
+        img {
+        }
+      }
+      .invite-link {
+        display: flex;
+        img {
+        }
+        p {
+        }
+      }
+    }
+  }
+
+  .main-header {
+    grid-column: 2 / 3;
+    grid-row: 1 / 2;
+    background: thistle;
+    .header-title {
+      p {
+      }
+    }
+    .header-buttons {
+      button.star {
+        img {
+        }
+      }
+      button.user {
+        img {
+        }
+      }
+      button.pin {
+        img {
+        }
+      }
+      button.topic {
+        img {
+        }
+      }
+    }
+    .header-tools {
+      button.tel {
+        img {
+        }
+      }
+      button.info {
+        img {
+        }
+      }
+      button.setting {
+        img {
+        }
+      }
+    }
+  }
+
+  .sub-header {
+    grid-column: 3 / 4;
+    grid-row: 1 / 2;
+    background: indianred;
+    .header-search {
+      img {
+      }
+      input {
+      }
+    }
+    .header-tools {
+      button.mention {
+        img {
+        }
+      }
+      button.star {
+        img {
+        }
+      }
+      button.other {
+        img {
+        }
+      }
+    }
+  }
+
+  main {
+    grid-column: 2 / 3;
+    grid-row: 2 / 4;
+    background: yellowgreen;
+    .date-line {
+      p {
+      }
+    }
+    .main-message {
+      .profile-icon {
+        img {
+        }
+      }
+      .user-status {
+        p.name {
+        }
+        span {
+        }
+        p.time {
+        }
+      }
+    }
+  }
+
+  .submit {
+    grid-column: 2 / 3;
+    grid-row: 4 / 5;
+    background: indianred;
+    .submit-area {
+      .icon {
+        img.clip-icon {
+        }
+        img.mention-icon {
+        }
+        img.smile-icon {
+        }
+      }
+    }
+    input {
+    }
+    button.submit-button {
+    }
+  }
+
+  .sled-header {
+    grid-column: 3 / 4;
+    grid-row: 2 / 3;
+    background: olive;
+    .sled-title {
+    }
+    .channel-name {
+    }
+    button.sled-close {
+    }
+  }
+  .sled-area {
+    grid-column: 3 / 4;
+    grid-row: 3 / 5;
+    background: lavender;
+    .sled-message {
+      .profile-icon {
+        img {
+        }
+      }
+      .user-status {
+        p.name {
+        }
+        span {
+        }
+        p.time {
+        }
+      }
+      .text {
+      }
+      .reactions {
+        button {
+          .reaction {
+          }
+        }
+      }
+    }
+    .reply-line {
+      p {
+      }
+    }
+  }
+  .sled-submit {
+    .submit-area {
+      .icon {
+      }
+      input {
+      }
+    }
+    .and-channel {
+      label {
+        input {
+        }
+        p {
+        }
+      }
+      a {
+        img.question-icon {
+        }
+      }
+    }
+    button.submit-button {
+    }
+  }
+}
+</style>
